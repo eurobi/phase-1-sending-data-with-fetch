01ac3ca5d477b1c0ff68ab39fba106a298ff8961
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-08-17 15:08:27 -0400

    commit tree 8cc19507388cdb0a3f26bb3890df8a15a5cfc325

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            const headerId = document.createElement('h1')
+            headerId.innerHTML = data.id 
+            document.querySelector('body').appendChild(headerId)
+            
+        })
+        .catch((error) => {
+            const eMessage = document.createElement('h1')
+            eMessage.innerHTML = error
+            document.querySelector('body').appendChild(eMessage)
+        })
+}
